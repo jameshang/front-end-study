3b6e89f35216a12f9c4099532f0f509485de45e2
James <james.hang@hotmail.com>
2018-09-29 16:32:16 +0800

    React route study

--- react-study/src/index.js
+++ react-study/src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { Hello, Clock, Counter, Region } from "./components";
 import registerServiceWorker from './registerServiceWorker';
-import PropTypes from 'prop-types';
+import { Routes } from './routes';
 
-Hello.propTypes = {
-    title: PropTypes.string
-}
+ReactDOM.render(<Routes />, document.getElementById('root'));
 
-var title = "James";
-var regionSource = "http://localhost:3000/region.json";
-
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Hello title={title} />, document.getElementById('hello'));
-ReactDOM.render(<Clock />, document.getElementById('clock'));
-ReactDOM.render(<Counter />, document.getElementById('counter'));
-ReactDOM.render(<Region source={regionSource} />, document.getElementById('region'));
 registerServiceWorker();
